68cafaeb2f07a96efd58fd524e63dd4640b39c1f
Abhinavgu27 <abhinavsinghal29oct@gmail.com>
2026-03-16 14:26:53 +0530

    Fixed nested git and uploaded actual server files

---
+++ griin-server/src/main/kotlin/com/abhinav/griinserver/HazardController.kt
@@ -0,0 +1,24 @@
+package com.abhinav.griinserver
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/hazards")
+class HazardController(private val repository: HazardRepository) {
+
+    @PostMapping
+    fun createHazard(@RequestBody report: HazardReport): ResponseEntity<Void> {
+        repository.save(report)
+        println("🟢 HAZARD RECEIVED FROM APP!!! Latitude: ${report.latitude}")
+        return ResponseEntity.ok().build()
+    }
+    @GetMapping
+    fun getAllHazards(): ResponseEntity<List<HazardReport>> {
+        val hazards = repository.findAll()
+        return ResponseEntity.ok(hazards) // This sends the full list from Neon to the app
+    }
+}
